3c6a718524bc2b2c45a873e2e6c4b5b0e4660ed3
Juan Lavalle <juanlavalle.jp@gmail.com>
2023-01-13 20:55:11 +0100

    13/01-sin actualizaciones

--- ft_split.c
+++ ft_split.c
@@ -10,4 +10,63 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+int	nb_words(char const *s, char c)
+{
+	int		cont;
+
+	cont = 0;
+	while (*s)
+	{
+		if (*s == c)
+			cont++;
+		s++;
+	}
+	return (cont);
+}
+
+int	length(const char *s, int start, char c)
+{
+	int		cont;
+
+	cont = 0;
+	while (s[start] != '\0')
+	{
+		if (s[start] == c)
+			break ;
+		start++;
+		cont++;
+	}
+	return (cont);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+	int		k;
+	int		i;
+	int		j;
+
+	if (!s || !c)
+		return (NULL);
+	res = (char **)malloc((nb_words(s, c) + 1) * sizeof(char));
+	if (!res)
+		return (NULL);
+	i = 0;
+	j = 0;
+	k = 0;
+	while (i < nb_words(s, c))
+	{
+		res[i] = (char *)malloc((length(s, i, c) + 1) * sizeof(char));
+		while (s[k] != c)
+		{
+			res[i][j] = s[k];
+			j++;
+			k++;
+		}
+		j = 0;
+		i++;
+	}
+	return (res);
+}
