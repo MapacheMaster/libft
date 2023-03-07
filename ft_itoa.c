c4ca4896b2c0bf72846a1dd8371687c8224041c7
Juan Lavalle <juanlavalle.jp@gmail.com>
2023-03-07 20:02:33 +0100

    v2.4-07/03-terminado itoa

---
+++ ft_itoa.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlavalle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 06:10:56 by jlavalle          #+#    #+#             */
+/*   Updated: 2023/02/15 06:11:01 by jlavalle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	get_len(int n)
+{
+	int	len;
+
+	len = 0;
+	if (n == -2147483648)
+		return (11);
+	if (n < 0)
+	{
+		n *= -1;
+		len++;
+	}
+	if (n == 0)
+		return (1);
+	while (n > 0)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*res;
+	int		len;
+
+	len = get_len(n);
+	res = (char *) ft_calloc(len + 1, sizeof (char));
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	if (!res)
+		return (NULL);
+	len--;
+	if (n < 0)
+	{
+		res[0] = '-';
+		n *= -1;
+	}
+	while (n > 9)
+	{
+		res[len] = n % 10 + 48;
+		n /= 10;
+		len--;
+	}
+	res[len--] = n + 48;
+	return (res);
+}
--- libft.h
+++ libft.h
@@ -41,5 +41,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
